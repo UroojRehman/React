04db16915282a82d1073c6d987e4d98fe664815d
Urooj-Rehman@97 <urooj_rehman@aptechnorth.edu.pk>
2026-07-15 21:46:24 +0500

    First Lecture

---
+++ myproject/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import hero from "./assets/hero.png";
+const App = () => {
+
+  return (
+    // fragment / wrapper <></>
+
+    <>
+      <h1>Hello, React!!!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto natus corporis, obcaecati omnis quis voluptatibus debitis maiores, earum, sint magni saepe suscipit cupiditate impedit asperiores ea ratione unde. Corporis odio quae, qui neque aspernatur error animi consequatur sapiente sint nihil architecto ipsum dolor provident tempore nisi alias sed? Commodi labore ullam, asperiores deserunt aspernatur saepe, hic sed rerum non praesentium consequatur perspiciatis expedita totam consectetur ea aperiam. Enim, reprehenderit quis deserunt dolores praesentium nobis nisi, beatae doloribus neque accusamus deleniti. Optio, quibusdam debitis. Molestiae, corporis vel,
+        nulla eius dicta quas ad suscipit alias fugit quia qui architecto nesciunt provident accusamus.</p>
+      <img src={hero} width={100} height={100}/>
+    </>
+
+  )
+}
+export default App;
+
